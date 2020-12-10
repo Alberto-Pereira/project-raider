1e45c28d0432c630392d3eb3fbb65323b2689778
Alberto-Pereira <alberto.pereira.gyn@gmail.com>
2020-12-10 16:25:23 -0300

    Câmera segue o jogador
    - A câmera segue o jogador
    - Possui Lerp
    
    - Próximas metas:
    - Implementar drop de moedas (cristais) e vida
    - Implementar sons do ambiente
    - Desenhar cenário

---
+++ Project Raider/Assets/Scripts/Player/PlayerCameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraController : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    
+    [SerializeField] private float speed;
+    
+    // Update is called once per frame
+    void Update () {
+        
+        Vector3 position = this.transform.position;
+        
+        position.y = Mathf.Lerp(this.transform.position.y, player.transform.position.y, speed * Time.deltaTime);
+        position.x = Mathf.Lerp(this.transform.position.x, player.transform.position.x, speed * Time.deltaTime);
+        
+        this.transform.position = position;
+    }
+}
